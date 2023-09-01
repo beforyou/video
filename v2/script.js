bea7fd875e5dbfa7551beb07f5c698ab637f23c0
beforyou <50078025+beforyou@users.noreply.github.com>
2023-09-01 15:41:47 +0800

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -90,7 +90,7 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   let mediaInput = document.querySelector('#media-url')
   let mediaURL = mediaInput.value
-  let api = "https://danmu666.8old.cn/jx.php?url="
+  let api = "https://videocdn.ihelpy.net/jiexi/m1907.html?m1907jx="
   let card = document.querySelector('.card')
   let player = document.querySelector('.player')
 
